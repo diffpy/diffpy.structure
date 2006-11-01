4b0b97503f5248de3fd38cde5d84ec1914de3577
Pavol Juhas <pavol.juhas@gmail.com>
2006-11-01 01:33:52 +0000

    Added "copy constructor" from existing Atom instance.

--- Structure/Atom.py
+++ Structure/Atom.py
@@ -16,7 +16,7 @@
 
 __id__ = "$Id$"
 
-import numpy as num
+import numpy
 
 class Atom:
     """Atom --> class for storing atom information
@@ -25,40 +25,60 @@ class Atom:
         element   -- type of the atom
         xyz       -- fractional coordinates
         occupancy -- fractional occupancy
-        U         -- matrix of temperature coefficients
+        U         -- anisotropic thermal displacement coefficients
         name      -- atom label
     """
 
-    def __init__(self, element, xyz, name='', occupancy=1.0, U=None):
-        """create atom of a specified type at given lattice coordinates"""
-        self.element = element
-        self.xyz = num.array(xyz, dtype=float)
-        self.name = name
-        if U is None:
-            self.U = num.zeros((3,3), dtype=float)
+    def __init__(self, atype, xyz=None, name=None, occupancy=None, U=None):
+        """Create atom of a specified type at given lattice coordinates.
+        Atom(a) creates a copy of Atom instance a.
+
+        atype       -- element symbol string or Atom instance
+        xyz         -- fractional coordinates
+        name        -- atom label
+        occupancy   -- fractional occupancy
+        U           -- anisotropic thermal displacement coefficients
+        """
+        # declare data members
+        self.element = None
+        self.xyz = numpy.zeros(3, dtype=float)
+        self.name = ''
+        self.occupancy = 1.0
+        self.U = numpy.zeros((3,3), dtype=float)
+        # assign them as needed
+        if isinstance(atype, Atom):
+            self.element = atype.element
+            self.xyz = numpy.array(atype.xyz)
+            self.name = atype.name
+            self.occupancy = atype.occupancy
+            self.U = numpy.array(atype.U)
         else:
-            self.U = num.array(U, dtype=float)
-        self.occupancy = occupancy
+            self.element = atype
+        # take care of remaining arguments
+        if xyz is not None:         self.xyz = numpy.array(xyz)
+        if name is not None:        self.name = name
+        if occupancy is not None:   self.occupancy = float(occupancy)
+        if U is not None:           self.U = numpy.array(U)
         return
 
     def Uiso(self):
         """equivalent isotropic temperature factor"""
-        Uiso = num.trace(self.U)/3.0
+        Uiso = numpy.trace(self.U)/3.0
         return Uiso
 
     def Biso(self):
         """equivalent isotropic Debye-Waler temperature factor"""
-        Biso = 8 * num.pi**2 * self.Uiso()
+        Biso = 8 * numpy.pi**2 * self.Uiso()
         return Biso
 
     def setUiso(self, Uiso):
         """set temperature coefficients matrix U to Uiso*identity(3)"""
-        self.U = Uiso * num.identity(3, dtype=float)
+        self.U = Uiso * numpy.identity(3, dtype=float)
         return
 
     def setBiso(self, Biso):
         """set matrix U to isotropic Debye-Waler factor Biso"""
-        self.U = self.setUiso( Biso/(8.0*num.pi**2) )
+        self.U = self.setUiso( Biso/(8.0*numpy.pi**2) )
         return
 
     def __repr__(self):
