9798274a0ab6fa0c5b388bd28c66f0f338b23cb9
Pavol Juhas <pavol.juhas@gmail.com>
2006-11-05 19:46:23 +0000

    Rewritten setup - diffpy distribution is now done with one python process, which merges setup arguments from all diffpy subpackages.

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-
+# version
 __id__ = "$Id$"
 
+from distutils.core import setup
+import sys
+import os.path
+
+thisfile = os.path.abspath(locals().get('__file__', 'setup.py'))
+setup_dir = os.path.dirname(thisfile)
+
+sys.path.insert(0, setup_dir)
 from Structure import __version__
+sys.path.pop(0)
 package_version = __version__
 
 # define distribution
-distribution = setup(
-    name = "Structure",
-    description = "Crystal structure container.",
-    version = package_version,
-    packages = [ "diffpy.Structure", "diffpy.Structure.Parsers" ],
-    package_dir = { "diffpy.Structure" : "Structure" },
-    scripts = [ "applications/anyeye", "applications/transtru" ]
-)
+setup_args = {
+    "name" : "Structure",
+    "description" : "Crystal structure container.",
+    "version" : package_version,
+    "packages" : [ "diffpy.Structure", "diffpy.Structure.Parsers" ],
+    "package_dir" : {
+        "diffpy.Structure" : os.path.join(setup_dir, "Structure")
+        },
+    "scripts" : [
+        os.path.join(setup_dir, "applications/anyeye"),
+        os.path.join(setup_dir, "applications/transtru")
+        ],
+}
+
+if __name__ == "__main__":
+    distribution = setup(**setup_args)
+
+# End of file 
