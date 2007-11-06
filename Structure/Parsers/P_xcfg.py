5897b0af83afec9a29d3e47c28244417257f7818
Pavol Juhas <pavol.juhas@gmail.com>
2007-11-06 17:52:41 +0000

    Removed confusing module PeriodicTable.py.  AtomicMass dictionary moved inside the `P_xcfg.py` module, because that is the only place, where it is used.

--- Structure/Parsers/P_xcfg.py
+++ Structure/Parsers/P_xcfg.py
@@ -25,6 +25,131 @@ from import_helper import InvalidStructureFormat
 from import_helper import isfloat
 from StructureParser import StructureParser
 
+
+########################################################################
+# Constants
+
+# Atomic Mass of elements
+# This can be later when PeriodicTable package becomes available.
+
+AtomicMass = {
+    "H"  : 1.007947,    # 1 H hydrogen 1.007947
+    "He" : 4.0026022,   # 2 He helium 4.0026022
+    "Li" : 6.9412,      # 3 Li lithium 6.9412
+    "Be" : 9.0121823,   # 4 Be beryllium 9.0121823
+    "B"  : 10.8117,     # 5 B boron 10.8117
+    "C"  : 12.01078,    # 6 C carbon 12.01078
+    "N"  : 14.00672,    # 7 N nitrogen 14.00672
+    "O"  : 15.99943,    # 8 O oxygen 15.99943
+    "F"  : 18.99840325, # 9 F fluorine 18.99840325
+    "Ne" : 20.17976,    # 10 Ne neon 20.17976
+    "Na" : 22.9897702,  # 11 Na sodium 22.9897702
+    "Mg" : 24.30506,    # 12 Mg magnesium 24.30506
+    "Al" : 26.9815382,  # 13 Al aluminium 26.9815382
+    "Si" : 28.08553,    # 14 Si silicon 28.08553
+    "P"  : 30.9737612,  # 15 P phosphorus 30.9737612
+    "S"  : 32.0655,     # 16 S sulfur 32.0655
+    "Cl" : 35.4532,     # 17 Cl chlorine 35.4532
+    "Ar" : 39.9481,     # 18 Ar argon 39.9481
+    "K"  : 39.09831,    # 19 K potassium 39.09831
+    "Ca" : 40.0784,     # 20 Ca calcium 40.0784
+    "Sc" : 44.9559108,  # 21 Sc scandium 44.9559108
+    "Ti" : 47.8671,     # 22 Ti titanium 47.8671
+    "V"  : 50.94151,    # 23 V vanadium 50.94151
+    "Cr" : 51.99616,    # 24 Cr chromium 51.99616
+    "Mn" : 54.9380499,  # 25 Mn manganese 54.9380499
+    "Fe" : 55.8452,     # 26 Fe iron 55.8452
+    "Co" : 58.9332009,  # 27 Co cobalt 58.9332009
+    "Ni" : 58.69342,    # 28 Ni nickel 58.69342
+    "Cu" : 63.5463,     # 29 Cu copper 63.5463
+    "Zn" : 65.4094,     # 30 Zn zinc 65.4094
+    "Ga" : 69.7231,     # 31 Ga gallium 69.7231
+    "Ge" : 72.641,      # 32 Ge germanium 72.641
+    "As" : 74.921602,   # 33 As arsenic 74.921602
+    "Se" : 78.963,      # 34 Se selenium 78.963
+    "Br" : 79.9041,     # 35 Br bromine 79.9041
+    "Kr" : 83.7982,     # 36 Kr krypton 83.7982
+    "Rb" : 85.46783,    # 37 Rb rubidium 85.46783
+    "Sr" : 87.621,      # 38 Sr strontium 87.621
+    "Y"  : 88.905852,   # 39 Y yttrium 88.905852
+    "Zr" : 91.2242,     # 40 Zr zirconium 91.2242
+    "Nb" : 92.906382,   # 41 Nb niobium 92.906382
+    "Mo" : 95.942,      # 42 Mo molybdenum 95.942
+    "Tc" : 98.0,        # 43 Tc technetium 98
+    "Ru" : 101.072,     # 44 Ru ruthenium 101.072
+    "Rh" : 102.905502,  # 45 Rh rhodium 102.905502
+    "Pd" : 106.421,     # 46 Pd palladium 106.421
+    "Ag" : 107.86822,   # 47 Ag silver 107.86822
+    "Cd" : 112.4118,    # 48 Cd cadmium 112.4118
+    "In" : 114.8183,    # 49 In indium 114.8183
+    "Sn" : 118.7107,    # 50 Sn tin 118.7107
+    "Sb" : 121.7601,    # 51 Sb antimony 121.7601
+    "Te" : 127.603,     # 52 Te tellurium 127.603
+    "I"  : 126.904473,  # 53 I iodine 126.904473
+    "Xe" : 131.2936,    # 54 Xe xenon 131.2936
+    "Cs" : 132.905452,  # 55 Cs caesium 132.905452
+    "Ba" : 137.3277,    # 56 Ba barium 137.3277
+    "La" : 138.90552,   # 57 La lanthanum 138.90552
+    "Ce" : 140.1161,    # 58 Ce cerium 140.1161
+    "Pr" : 140.907652,  # 59 Pr praseodymium 140.907652
+    "Nd" : 144.243,     # 60 Nd neodymium 144.243
+    "Pm" : 145.0,       # 61 Pm promethium 145
+    "Sm" : 150.363,     # 62 Sm samarium 150.363
+    "Eu" : 151.9641,    # 63 Eu europium 151.9641
+    "Gd" : 157.253,     # 64 Gd gadolinium 157.253
+    "Tb" : 158.925342,  # 65 Tb terbium 158.925342
+    "Dy" : 162.5001,    # 66 Dy dysprosium 162.5001
+    "Ho" : 164.930322,  # 67 Ho holmium 164.930322
+    "Er" : 167.2593,    # 68 Er erbium 167.2593
+    "Tm" : 168.934212,  # 69 Tm thulium 168.934212
+    "Yb" : 173.043,     # 70 Yb ytterbium 173.043
+    "Lu" : 174.9671,    # 71 Lu lutetium 174.9671
+    "Hf" : 178.492,     # 72 Hf hafnium 178.492
+    "Ta" : 180.94791,   # 73 Ta tantalum 180.94791
+    "W"  : 183.841,     # 74 W tungsten 183.841
+    "Re" : 186.2071,    # 75 Re rhenium 186.2071
+    "Os" : 190.233,     # 76 Os osmium 190.233
+    "Ir" : 192.2173,    # 77 Ir iridium 192.2173
+    "Pt" : 195.0782,    # 78 Pt platinum 195.0782
+    "Au" : 196.966552,  # 79 Au gold 196.966552
+    "Hg" : 200.592,     # 80 Hg mercury 200.592
+    "Tl" : 204.38332,   # 81 Tl thallium 204.38332
+    "Pb" : 207.21,      # 82 Pb lead 207.21
+    "Bi" : 208.980382,  # 83 Bi bismuth 208.980382
+    "Po" : 209.0,       # 84 Po polonium 209
+    "At" : 210.0,       # 85 At astatine 210
+    "Rn" : 222.0,       # 86 Rn radon 222
+    "Fr" : 223.0,       # 87 Fr francium 223
+    "Ra" : 226.0,       # 88 Ra radium 226
+    "Ac" : 227.0,       # 89 Ac actinium 227
+    "Th" : 232.03811,   # 90 Th thorium 232.03811
+    "Pa" : 231.035882,  # 91 Pa protactinium 231.035882
+    "U"  : 238.028913,  # 92 U uranium 238.028913
+    "Np" : 237.0,       # 93 Np neptunium 237
+    "Pu" : 244.0,       # 94 Pu plutonium 244
+    "Am" : 243.0,       # 95 Am americium 243
+    "Cm" : 247.0,       # 96 Cm curium 247
+    "Bk" : 247.0,       # 97 Bk berkelium 247
+    "Cf" : 251.0,       # 98 Cf californium 251
+    "Es" : 252.0,       # 99 Es einsteinium 252
+    "Fm" : 257.0,       # 100 Fm fermium 257
+    "Md" : 258.0,       # 101 Md mendelevium 258
+    "No" : 259.0,       # 102 No nobelium 259
+    "Lr" : 262.0,       # 103 Lr lawrencium 262
+    "Rf" : 261.0,       # 104 Rf rutherfordium 261
+    "Db" : 262.0,       # 105 Db dubnium 262
+    "Sg" : 266.0,       # 106 Sg seaborgium 266
+    "Bh" : 264.0,       # 107 Bh bohrium 264
+    "Hs" : 277.0,       # 108 Hs hassium 277
+    "Mt" : 268.0,       # 109 Mt meitnerium 268
+    "Ds" : 281.0,       # 110 Ds darmstadtium 281
+    "Rg" : 272.0,       # 111 Rg roentgenium 272
+}
+
+# End of Constants
+
+
+########################################################################
 class P_xcfg(StructureParser):
     """Parser for AtomEye extended CFG format.
     """
