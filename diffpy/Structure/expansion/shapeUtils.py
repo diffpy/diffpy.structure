2a9e5a052f93fa31996d654f747078004e3f5074
Pavol Juhas <pavol.juhas@gmail.com>
2009-04-02 00:14:33 +0000

    Fixed formatting of source headers, no change in the code. File encoding must be defined on first or second line, PEP-0263.

--- diffpy/Structure/expansion/shapeUtils.py
+++ diffpy/Structure/expansion/shapeUtils.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
+##############################################################################
+#
+# PDFgui            by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2008 trustees of the Michigan State University.
+#                   All rights reserved.
+#
+# File coded by:    Chris Farrow
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+
 """Utilities for making shapes."""
 
 __id__ = "$Id$"
