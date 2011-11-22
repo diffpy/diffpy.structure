33fc3fcbdca7a116e620f386e885a761d115c7b7
Pavol Juhas <pavol.juhas@gmail.com>
2011-11-22 00:56:27 +0000

    create self.pdffit before calling Structure.__init__ because pdffit can be assigned when loading a Structure.

--- diffpy/Structure/pdffitstructure.py
+++ diffpy/Structure/pdffitstructure.py
@@ -32,22 +32,19 @@ class PDFFitStructure(Structure):
     """
 
     def __init__(self, *args, **kwargs):
+        self.pdffit = {
+            'scale' : 1.0,
+            'delta1' : 0.0,
+            'delta2' : 0.0,
+            'sratio' : 1.0,
+            'rcut' : 0.0,
+            'spcgr' : 'P1',
+            'spdiameter' : 0.0,
+            'stepcut' : 0.0,
+            'dcell' : 6*[0.0],
+            'ncell' : [1, 1, 1, 0],
+        }
         Structure.__init__(self, *args, **kwargs)
-        # do not overwrite self.pdffit, when instantiated as a copy
-        # of existing PDFFitStructure
-        if not self.pdffit:
-            self.pdffit = {
-                'scale' : 1.0,
-                'delta1' : 0.0,
-                'delta2' : 0.0,
-                'sratio' : 1.0,
-                'rcut' : 0.0,
-                'spcgr' : 'P1',
-                'spdiameter' : 0.0,
-                'stepcut' : 0.0,
-                'dcell' : 6*[0.0],
-                'ncell' : [1, 1, 1, 0],
-            }
         return
 
     def read(self, filename, format='auto'):
