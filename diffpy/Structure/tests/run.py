d0f432c5da2ff53c7652781ff2e423b6cd1d19c5
Pavol Juhas <pavol.juhas@gmail.com>
2012-03-23 02:38:12 +0000

    Convenience module for running all unit tests.

---
+++ diffpy/Structure/tests/run.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy.Structure  by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2010 Trustees of the Columbia University
+#                   in the City of New York.  All rights reserved.
+#
+# File coded by:    Pavol Juhas
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE.txt for license information.
+#
+##############################################################################
+
+"""Convenience module for executing all unit tests with
+
+python -m diffpy.Structure.tests.run
+"""
+
+# version
+__id__ = '$Id$'
+
+
+if __name__ == '__main__':
+    import sys
+    from diffpy.Structure.tests import test
+    # produce zero exit code for a successful test
+    sys.exit(not test().wasSuccessful())
+
+# End of file
