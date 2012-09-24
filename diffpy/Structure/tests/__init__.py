573dde9da64555fc66f32f856f6ba4a96dc419a9
Pavol Juhas <pavol.juhas@gmail.com>
2012-09-24 18:07:54 +0000

    added facility for running unit tests for the PDFgui dependencies.

--- diffpy/Structure/tests/__init__.py
+++ diffpy/Structure/tests/__init__.py
@@ -19,9 +19,11 @@
 # version
 __id__ = '$Id$'
 
-def test():
-    '''Execute all unit tests for the diffpy.Structure package.
-    Return a unittest TestResult object.
+
+def testsuite():
+    '''Build a unit tests suite for the diffpy.Structure package.
+
+    Return a unittest.TestSuite object.
     '''
     import unittest
     modulenames = '''
@@ -39,8 +41,18 @@ def test():
     for mname in modulenames:
         exec ('import %s as mobj' % mname)
         suite.addTests(loader.loadTestsFromModule(mobj))
+    return suite
+
+
+def test():
+    '''Execute all unit tests for the diffpy.Structure package.
+    Return a unittest TestResult object.
+    '''
+    import unittest
+    suite = testsuite()
     runner = unittest.TextTestRunner()
     result = runner.run(suite)
     return result
+
 
 # End of file
