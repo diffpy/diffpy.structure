79dd1a494d1e7685106b15a580d70a74ce12c5b9
Pavol Juhas <pavol.juhas@gmail.com>
2013-10-18 18:13:24 -0400

    Remove "g" prefix from the __gitsha__ value.

--- diffpy/Structure/version.py
+++ diffpy/Structure/version.py
@@ -13,7 +13,8 @@
 #
 ##############################################################################
 
-"""Definition of __version__ and __date__ for diffpy.Structure.
+
+"""Definition of __version__, __date__, __gitsha__ for diffpy.Structure.
 """
 
 
@@ -26,7 +27,6 @@ __date__ = __version__[-8:-4] + '-' + \
            __version__[-4:-2] + '-' + __version__[-2:]
 
 # GIT SHA hash is the second last component in the version string
-__gitsha__ = __version__.rsplit('-', 2)[-2]
-
+__gitsha__ = __version__.rsplit('-', 2)[-2][1:]
 
 # End of file
