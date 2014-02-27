3cd176609c78ae99b4553fc59c05dbfe1a821f7e
Pavol Juhas <pavol.juhas@gmail.com>
2014-02-26 21:27:20 -0500

    typo in classifiers

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ setup_args = dict(
             'Operating System :: POSIX',
             'Operating System :: Unix',
             'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7'
+            'Programming Language :: Python :: 2.7',
             'Topic :: Scientific/Engineering :: Chemistry',
             'Topic :: Scientific/Engineering :: Physics',
         ],
