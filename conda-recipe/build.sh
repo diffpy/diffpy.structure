ca0a819c40b708dfe190aa846496c0efed84446b
Pavol Juhas <pavol.juhas@gmail.com>
2016-09-01 15:11:25 -0400

    Stop using deprecated __conda_version__.txt file.
    Use jinja template function to get version string from setup.py.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -4,8 +4,5 @@ $PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
 
-$PYTHON setup.py --version > __conda_version__.txt
-
-# See
-# http://docs.continuum.io/conda/build.html
+# See http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
