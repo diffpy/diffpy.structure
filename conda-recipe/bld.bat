2cae319ddc15f27da063bf9ca650b35993aeacef
Pavol Juhas <pavol.juhas@gmail.com>
2016-09-22 11:50:40 -0400

    Remove __conda_version__ from Windows build script.
    Not used anymore, version is defined in meta.yaml.

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -3,8 +3,5 @@ if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
 
-"%PYTHON%" setup.py --version > __conda_version__.txt
-
-:: See
-:: http://docs.continuum.io/conda/build.html
+:: See http://docs.continuum.io/conda/build.html
 :: for a list of environment variables that are set during the build process.
