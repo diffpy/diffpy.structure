a0c6a672c38033d1a3b7fc584ffba81b80cc2b5d
Pavol Juhas <pavol.juhas@gmail.com>
2017-06-07 00:57:24 -0400

    REL: go back to unversioned pycifrw in setup.py.
    PyCifRW 4.3.0 works with latest releases of Python 2.7.
    This reverts commit 51d6f09d0a27fee8d40427847b4c17645df151ad.

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup_args = dict(
     include_package_data = True,
     zip_safe = False,
     install_requires = [
-        'pycifrw != 4.3.0',
+        'pycifrw',
     ],
 
     author = 'Simon J.L. Billinge group',
