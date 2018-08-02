569fcaed429495b2d43383c6eb8ca2bcc86de5a8
Pavol Juhas <pavol.juhas@gmail.com>
2018-08-02 12:08:51 -0400

    TST: enable warnings in our test script

--- src/diffpy/structure/tests/run.py
+++ src/diffpy/structure/tests/run.py
@@ -21,6 +21,12 @@ python -m diffpy.structure.tests.run
 
 if __name__ == '__main__':
     import sys
+    # show warnings by default
+    if not sys.warnoptions:
+        import os, warnings
+        warnings.simplefilter("default")
+        # also affect subprocesses
+        os.environ["PYTHONWARNINGS"] = "default"
     from diffpy.structure.tests import test
     # produce zero exit code for a successful test
     sys.exit(not test().wasSuccessful())
