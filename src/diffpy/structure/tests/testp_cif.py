dfcb2aacc66e30a861468304da1e076536adf680
Pavol Juhas <pavol.juhas@gmail.com>
2019-07-01 16:19:04 -0400

    TST: parsing of CIF with curly brackets value

--- src/diffpy/structure/tests/testp_cif.py
+++ src/diffpy/structure/tests/testp_cif.py
@@ -344,6 +344,15 @@ class TestP_cif(unittest.TestCase):
         return
 
 
+    @unittest.expectedFailure
+    def test_curly_brace(self):
+        "verify loading of a CIF file with unquoted curly brace"
+        ptest = self.ptest
+        stru = ptest.parseFile(datafile('curlybrackets.cif'))
+        self.assertEqual(20, len(stru))
+        return
+
+
     def test_getParser(self):
         """Test passing of eps keyword argument by getParser function.
         """
