1d042840648a3ca438340c385b68edfc643ca889
Pavol Juhas <pavol.juhas@gmail.com>
2019-07-05 11:47:05 -0400

    DEP: support old import name for applications
    Allow their import with DeprecationWarning.

---
+++ src/diffpy/structure/applications/anyeye.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+Temporary support for old import name.
+
+This module is deprecated and will be removed in version 3.1.
+"""
+
+# TODO remove this module in version 3.1.
+
+from diffpy.structure.apps.anyeye import main
+
+if __name__ == "__main__":
+    main()
