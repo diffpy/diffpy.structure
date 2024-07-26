cb0e8bfe9cc57f334928e39d3f23ea57e53df28a
Tieqiong Zhang <tz2600@columbia.edu>
2024-07-26 13:08:26 -0400

    modify doc/conf.py, reflect changes in test file

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,6 +18,8 @@ import time
 from importlib.metadata import version
 from pathlib import Path
 
+autodoc_mock_imports = ["importlib.metadata"]
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use Path().resolve() to make it absolute, like shown here.
