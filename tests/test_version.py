2df4a06417885a662a579971cb87eb6984f2bcfc
Sangjoon Bob Lee <bobleesj@gmail.com>
2024-09-23 09:28:36 -0400

    Remove failed msg for version attribute

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,5 +6,5 @@ import diffpy.structure
 
 def test_package_version():
     """Ensure the package version is defined and not set to the initial placeholder."""
-    assert hasattr(diffpy.structure, '__version__'), "The package version is not defined."
+    assert hasattr(diffpy.structure, '__version__')
     assert diffpy.structure.__version__ != "0.0.0"
