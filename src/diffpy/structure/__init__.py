4f7656c065c74f223168908d44b87d6fd74dfcfc
stevenhua0320 <r.hua@mail.utoronto.ca>
2026-03-14 01:13:13 -0400

    fix: fix load_structure method with loading Path object.

--- src/diffpy/structure/__init__.py
+++ src/diffpy/structure/__init__.py
@@ -34,6 +34,7 @@ Exceptions:
 """
 
 
+import os
 import sys
 
 import diffpy.structure as _structure
@@ -46,8 +47,17 @@ from diffpy.structure.structureerrors import LatticeError, StructureFormatError,
 
 # package version
 from diffpy.structure.version import __version__
+from diffpy.utils._deprecator import build_deprecation_message, deprecated
 
 # Deprecations -------------------------------------------------------
+base = "diffpy.structure"
+removal_version = "4.0.0"
+loadStructure_deprecation_msg = build_deprecation_message(
+    base,
+    "loadStructure",
+    "load_structure",
+    removal_version,
+)
 
 
 # @deprecated
@@ -72,7 +82,17 @@ sys.modules["diffpy.Structure"] = DeprecatedStructureModule()
 # top level routines
 
 
+@deprecated(loadStructure_deprecation_msg)
 def loadStructure(filename, fmt="auto", **kw):
+    """This function has been deprecated and will be removed in version
+    4.0.0.
+
+    Please use diffpy.structure.load_structure instead.
+    """
+    return load_structure(filename, fmt, **kw)
+
+
+def load_structure(filename, fmt="auto", **kw):
     """Load new structure object from the specified file.
 
     Parameters
@@ -96,7 +116,7 @@ def loadStructure(filename, fmt="auto", **kw):
         Return a more specific PDFFitStructure type for 'pdffit'
         and 'discus' formats.
     """
-
+    filename = os.fspath(filename)
     p = get_parser(fmt, **kw)
     rv = p.parse_file(filename)
     return rv
